a7315df4cb4a02df4debb4fc7cfed4c09ec04080
luicarsar <luicarsarg@gmail.com>
2023-11-27 08:23:58 +0100

    add: javascript

---
+++ archivo_script.js
@@ -0,0 +1,21 @@
+
+
+
+function ejecuta(){
+
+for(var i=0;i<3;i++){
+
+    /*document.getElementsByTagName("p") [i].onclick=saludo;}*/
+    /*document.getElementById("importante").onclick=saludo;*/
+  var z=document.getElementsByClassName("importante")[i].onclick=saludo;}
+
+
+}
+
+
+function saludo(){
+
+alert ("Hola de nuevo");
+}
+
+window.onload=ejecuta;
