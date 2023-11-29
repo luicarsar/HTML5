4f7f09c8e1900b4a8032169fe4cf8045bf7cfe9b
luicarsar <luicarsarg@gmail.com>
2023-11-29 08:20:29 +0100

    29/11/2023

--- archivo_script.js
+++ archivo_script.js
@@ -3,11 +3,20 @@
 
 function ejecuta(){
 
-for(var i=0;i<3;i++){
+
 
     /*document.getElementsByTagName("p") [i].onclick=saludo;}*/
     /*document.getElementById("importante").onclick=saludo;*/
-  var z=document.getElementsByClassName("importante")[i].onclick=saludo;}
+ /* var z=document.getElementsByClassName("importante")[i].onclick=saludo;}*/
+
+/*document.querySelector(".importante").onclick=saludo;*/
+/*document.querySelector("#principal p:last-child").onclick=saludo;*/
+/*var elementos=document.querySelectorAll("p");                      Selecionamos todos los párrafos*/
+var elementos=document.querySelectorAll("#principal p,span");         /*Seleccionamos sólo los div y la palabra del SPAN*/
+
+for(var i=0;i<elementos.length;i++){
+
+elementos[i].onclick=saludo;}
 
 
 }
